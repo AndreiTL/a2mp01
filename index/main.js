017151ddc87f17e7f3b6ad3c9b43f22a01f4e828
Andrei.Tsitou <andrei_tsitou@epam.com>
2016-11-25 18:05:02 +0300

    temp weather 010 - raw weather data is shown

--- index/main.js
+++ index/main.js
@@ -53,28 +53,15 @@
 
 	"use strict";
 	__webpack_require__(2);
-	// import './main.css'
-	// // import './index.scss';  // not work
-	// class Greeter {
-	//     constructor(public greeting: string) { }
-	//     greet() {
-	//         return "<h1>" + this.greeting + "</h1>";
-	//     }
-	// }
-	//
-	// var greeter = new Greeter("Hello, world!");
-	//
-	// document.body.innerHTML = greeter.greet();
 	var weather_service_1 = __webpack_require__(6);
-	var googlemap_service_1 = __webpack_require__(8);
+	var googlemap_service_1 = __webpack_require__(9);
 	var Main = (function () {
 	    function Main() {
+	        this.weatherService = new weather_service_1.WeatherService();
+	        this.googleMapService = new googlemap_service_1.GoogleMapService();
 	    }
 	    Main.prototype.renderData = function () {
-	        var weatherService = new weather_service_1.WeatherService();
-	        var googleMapService = new googlemap_service_1.GoogleMapService();
-	        var innerBlock = "<div> \n          <div>" + weatherService.getInner() + "</div>\n          <div>" + googleMapService.getInner() + "</div>\n      </div>";
-	        return innerBlock;
+	        return "<div> \n          <div class='weather'>" + this.weatherService.getInner() + "</div>\n          <div class='map'>" + this.googleMapService.getInner() + "</div>\n      </div>";
 	    };
 	    return Main;
 	}());
@@ -96,14 +83,66 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	"use strict";
-	__webpack_require__(7);
+	var rest_service_1 = __webpack_require__(7);
+	var location_service_1 = __webpack_require__(8);
 	var WeatherService = (function () {
 	    function WeatherService() {
-	        this.innerBlock = "<div> \n        <!--$ {this.dowloadButton} -->\n        <div> Wheather table will be here </div>\n        </div> ";
+	        this.API = "94c7919f6854ca11558382472a998f8f";
+	        this.cnt = '10';
+	        this.url = "http://api.openweathermap.org/data/2.5/weather?id=625144&APPID=" + this.API; // Minsk id
+	        this.type = 'GET';
+	        this.async = true;
+	        this.townTableTemp = '';
+	        this.townTableRender = 'Loading';
+	        this.innerBlock = "<div>        \n            <div>Weather in towns: </div>\n            <div class=\"townstable\"></div>\n        </div> ";
 	    }
-	    WeatherService.prototype.downloadweather = function () {
+	    WeatherService.prototype._initTable = function () {
+	        return "<div class='tablewrapper'>";
+	    };
+	    WeatherService.prototype._addLineToTable = function (tableQuery, row) {
+	        tableQuery.concat(row);
+	    };
+	    WeatherService.prototype._endTable = function () {
+	        return '</div>';
+	    };
+	    WeatherService.prototype.generateTableRow = function (rowObject) {
+	        return "<div class=\"tablerow\"> \n        <div class=\"cell townname\"><span>" + rowObject.name + "</span></div>\n        <div class=\"cell towntemp\"><span>" + (rowObject.main.temp - 274.15) + "</span></div>\n      </div>";
+	    };
+	    WeatherService.prototype.generateTownTable = function (array, context) {
+	        context.townTableTemp = context._initTable();
+	        array.forEach(function (value, index, array) {
+	            // context._addLineToTable(context.townTableTemp, context.generateTableRow(value));
+	            context.townTableTemp = context.townTableTemp.concat(context.generateTableRow(value));
+	        });
+	        // context._endTable(context.townTableTemp);
+	        context.townTableTemp = context.townTableTemp.concat(context._endTable());
+	        console.log(context.townTableTemp);
+	        context.townTableRender = String(context.townTableTemp);
+	        context.updateTowsTable(context);
+	    };
+	    WeatherService.prototype.updateTowsTable = function (context) {
+	        document.querySelector('.townstable').innerHTML = context.townTableRender;
+	    };
+	    WeatherService.prototype.downloadWeatherInCircle = function (coordinates, context) {
+	        var urlTemplate = "http://api.openweathermap.org/data/2.5/find?lat=" + coordinates.latitude + "&lon=" + coordinates.longitude + "&cnt=" + context.cnt + "&appid=" + context.API;
+	        console.log(urlTemplate);
+	        rest_service_1.RestService.sendRequest(context.type, urlTemplate, context.async, context.callBackResponseList, context, '');
+	    };
+	    WeatherService.prototype.callBackResponseList = function (data, context) {
+	        if (data !== null) {
+	            console.log(' data ' + data);
+	            context.weatherObject = JSON.parse(data);
+	            context.generateTownTable(context.weatherObject.list, context);
+	            context.updateTowsTable(context);
+	        }
+	        else {
+	            console.log('Cann\'t load data from weather portal!');
+	            alert('Cann\'t load data from weather portal!');
+	        }
 	    };
 	    WeatherService.prototype.getInner = function () {
+	        location_service_1.LocationService.getCurrentLocation(this.downloadWeatherInCircle, this);
+	        // this.downloadweather();
 	        return this.innerBlock;
 	    };
 	    return WeatherService;
@@ -115,64 +154,61 @@
 /* 7 */
 /***/ function(module, exports) {
 
-	// var Promise = require("es6-promise").Promise;
-	// require('es6-promise').polyfill();
-	// import * as Promise from 'es6-promise/Promise';
 	"use strict";
-	var RestService = (function () {
-	    function RestService() {
+	exports.RestService = {
+	    sendRequest: function (type, url, async, callBack, context, body) {
+	        var xhr = new XMLHttpRequest();
+	        xhr.open(type, url, async);
+	        xhr.send([body]);
+	        xhr.onreadystatechange = function () {
+	            if (this.readyState != 4)
+	                return;
+	            // console.log(this.readyState + "" + this.status);
+	            if (this.status != 200) {
+	                // console.log(this.status + ': ' + this.statusText);
+	                callBack(null);
+	            }
+	            else {
+	                // console.log(this.responseText);
+	                callBack(this.responseText, context);
+	            }
+	        };
 	    }
-	    return RestService;
-	}());
-	exports.RestService = RestService;
-	// import '../../../app';
-	// import IOptions = Bua.IOptions;
-	//
-	// import * as Promise from 'es6-promise/dist/es6-promise.js'
-	// import 'whatwg-fetch';
-	//
-	// export class RestService {
-	//
-	//   constructor() {
-	//
-	//   }
-	//
-	//   sendRequest(type, url, data) {
-	//     const lowerCaseType = type.toLowerCase();
-	//
-	//     const headers = lowerCaseType !== 'get' ? {
-	//       'Content-Type': 'application/json',
-	//       'Accept': 'application/json',
-	//     } : {};
-	//
-	//     const options: IOptions  = {
-	//       method: type,
-	//       headers,
-	//     };
-	//
-	//     if (data) {
-	//       options.body = JSON.stringify(data);
-	//     }
-	//
-	//     return fetch(url, options)
-	//       .then(response => {
-	//         // if (response.status === 401) {
-	//         //   notAuthorizedHandlers.forEach(handler => handler());
-	//         // }
-	//         // if (response.status !== 200) {
-	//         //   return Promise.reject(response.status);
-	//         // }
-	//         return response.json();
-	//       })
-	//       .catch(err => Promise.reject(err));
-	//
-	//
-	//   }
-	// } 
+	};
 
 
 /***/ },
 /* 8 */
+/***/ function(module, exports) {
+
+	"use strict";
+	exports.LocationService = {
+	    getCurrentLocation: function (callBack, context) {
+	        var options = {
+	            enableHighAccuracy: true,
+	            timeout: 5000,
+	            maximumAge: 0
+	        };
+	        function success(pos) {
+	            var crd = pos.coords;
+	            console.log('Your current position is:');
+	            console.log('Latitude : ' + crd.latitude);
+	            console.log('Longitude: ' + crd.longitude);
+	            console.log('More or less ' + crd.accuracy + ' meters.');
+	            callBack(crd, context);
+	            return;
+	        }
+	        function error(err) {
+	            console.warn('ERROR(' + err.code + '): ' + err.message);
+	            return;
+	        }
+	        navigator.geolocation.getCurrentPosition(success, error, options);
+	    }
+	};
+
+
+/***/ },
+/* 9 */
 /***/ function(module, exports) {
 
 	"use strict";
